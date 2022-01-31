3a25823c9c8a6ce83543322160be066aba6ca0e0
xuzebin <1594361357@qq.com>
2022-01-31 15:23:52 +0800

    first commit

---
+++ src/main/java/com/giteedemo/giteetest/GiteeTestApplication.java
@@ -0,0 +1,13 @@
+package com.giteedemo.giteetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiteeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiteeTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/giteedemo/giteetest/GiteeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giteedemo.giteetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiteeTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
