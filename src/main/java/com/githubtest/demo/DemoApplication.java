6f7db931324ac54147d8d1c585437497c22f577e
xuzebin <1594361357@qq.com>
2022-02-01 13:22:24 +0800

    hot-fix修改二

--- src/main/java/com/githubtest/demo/DemoApplication.java
+++ src/main/java/com/githubtest/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("jiayouya");
-        System.out.println("加速度");
+        System.out.println("加速度1111111");
     }
 
 }
